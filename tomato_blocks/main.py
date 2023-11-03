7bab640b0fefcb4428f520c4caecc728639ce928
Chris Birch <datapointchris@gmail.com>
2023-11-03 11:39:07 -0500

    refactor: Change tomato status fields from boolean to datetime

--- tomato_blocks/main.py
+++ tomato_blocks/main.py
@@ -10,20 +10,20 @@ from datetime import datetime
 import schedule
 import typer
 from rich import print
-from sqlalchemy import Boolean, Column, DateTime, Integer, String, create_engine, func
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, DateTime, Integer, String, create_engine, func
+from sqlalchemy.orm import Session, declarative_base
 from tabulate import tabulate
 
 from .tomato_block import TomatoBlock
 from .weekly_schedule import WeeklySchedule
 
-BASE_DIR = pathlib.Path().home().joinpath("code", "projects", "python", "tomato-blocks")
-EXIT_MESSAGE = "\n\nTimer Interrupted"
-SQLITE_DB = "tomato.db"
+BASE_DIR = pathlib.Path().home().joinpath('code', 'projects', 'python', 'tomato-blocks')
+EXIT_MESSAGE = '\n\nTimer Cancelled'
+SQLITE_DB = 'tomato.db'
 
 logger = logging.getLogger(__name__)
-formatter = logging.Formatter("%(message)s")
-file_handler = logging.FileHandler("tomato.log")
+formatter = logging.Formatter('%(message)s')
+file_handler = logging.FileHandler('tomato.log')
 file_handler.setFormatter(formatter)
 logger.setLevel(logging.INFO)
 logger.addHandler(file_handler)
@@ -31,133 +31,115 @@ logger.addHandler(file_handler)
 app = typer.Typer()
 
 
-engine = create_engine("postgresql://localhost/tomato")
+engine = create_engine('postgresql://localhost/tomato')
 
 Base = declarative_base()
 
 
-class Tomato(Base):
-    __tablename__ = "tomato"
+class Tomato(Base):  # type: ignore
+	__tablename__ = 'tomato'
 
-    id = Column(Integer, primary_key=True)
-    date = Column(DateTime)
-    uuid = Column(String)
-    title = Column(String)
-    started = Column(Boolean)
-    completed = Column(Boolean)
-    interrupted = Column(Boolean)
+	id = Column(Integer, primary_key=True)
+	uuid = Column(String, default=uuid.uuid4)
+	title = Column(String)
+	started_at = Column(DateTime, nullable=True)
+	completed_at = Column(DateTime, nullable=True)
+	cancelled_at = Column(DateTime, nullable=True)
 
 
 Base.metadata.create_all(engine)
 
 
-def log_to_sql(date, uid, title, started=False, completed=False, interrupted=False):
-    try:
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-        tomato_block = Tomato(
-            date=date,
-            uuid=uid,
-            title=title,
-            started=started,
-            completed=completed,
-            interrupted=interrupted,
-        )
-        session.add(tomato_block)
-        session.commit()
-    except Exception as e:
-        print(f"[red]\n\nLogging to SQL crashed:\n{e}[/red]")
-
-
 @app.command()
 def timer(title):
-    date = datetime.now()
-    uid = uuid.uuid4()
-    try:
-        logger.info(f"{date} | {uid} | {title} | started")
-        log_to_sql(date, str(uid), title, started=True)
-        TomatoBlock(title).run()
-        logger.info(f"{date} | {uid} | {title} | completed")
-        log_to_sql(date, str(uid), title, completed=True)
-    except KeyboardInterrupt:
-        logger.info(f"{date} | {uid} | {title} | interrupted")
-        log_to_sql(date, str(uid), title, interrupted=True)
-        print(EXIT_MESSAGE)
-        sys.exit(0)
+	with Session(engine) as session:
+		tomato = Tomato(title=title, started_at=datetime.now())
+		session.add(tomato)
+		session.commit()
+		session.refresh(tomato)
+		try:
+			TomatoBlock(title).run()
+			tomato.completed_at = datetime.now()
+			session.add(tomato)
+			session.commit()
+			session.refresh(tomato)
+		except KeyboardInterrupt:
+			tomato.cancelled_at = datetime.now()
+			session.add(tomato)
+			session.commit()
+			print(EXIT_MESSAGE)
+			sys.exit(0)
 
 
 @app.command()
 def update():
-    print("[blue]Updating tomato-blocks...[/blue]")
-    print(BASE_DIR)
-    os.chdir(BASE_DIR)
+	print('[blue]Updating tomato-blocks...[/blue]')
+	print(BASE_DIR)
+	os.chdir(BASE_DIR)
 
-    print("[green]Building new wheel...[/green]")
-    subprocess.call("poetry build", shell=True)
+	print('[green]Building new wheel...[/green]')
+	subprocess.call('poetry build', shell=True)
 
-    wheel_path = next(BASE_DIR.joinpath("dist").glob("*.whl"))
-    print("[green]Installing new wheel...[/green]")
-    print(f"[green]{wheel_path.name}[/green]")
-    subprocess.call(f"pip install --quiet --user {wheel_path} --force-reinstall", shell=True)
-    print("[green]Done![/green]")
+	wheel_path = next(BASE_DIR.joinpath('dist').glob('*.whl'))
+	print('[green]Installing new wheel...[/green]')
+	print(f'[green]{wheel_path.name}[/green]')
+	subprocess.call(f'pip install --quiet --user {wheel_path} --force-reinstall', shell=True)
+	print('[green]Done![/green]')
 
 
 @app.command()
 def daily():
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    rows = (
-        session.query(
-            func.date(Tomato.date).label("date"),
-            func.sum(func.cast(Tomato.started, Integer)).label("started"),
-            func.sum(func.cast(Tomato.completed, Integer)).label("completed"),
-            func.sum(func.cast(Tomato.interrupted, Integer)).label("interrupted"),
-        )
-        .group_by(func.date(Tomato.date))
-        .order_by(func.date(Tomato.date))
-        .all()
-    )
-    print("[green]Daily Summary[/green]")
-    print(tabulate(rows, headers=["Date", "Started", "Completed", "Interrupted"]))
+	with Session(engine) as session:
+		rows = (
+			session.query(
+				# func.extract('date', Tomato.started_at).label('date'),
+				func.date(Tomato.started_at).label('date'),
+				func.count(Tomato.started_at).label('started'),
+				func.count(Tomato.completed_at).label('completed'),
+				func.count(Tomato.cancelled_at).label('cancelled'),
+			)
+			.group_by(func.date(Tomato.started_at))
+			.order_by(func.date(Tomato.started_at))
+			.all()
+		)
+		print('[green]Daily Summary[/green]')
+		print(tabulate(rows, headers=['Date', 'Started', 'Completed', 'Interrupted']))
 
 
 @app.command()
 def weekly():
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    rows = (
-        session.query(
-            func.extract("week", Tomato.date).label("week"),
-            func.sum(func.cast(Tomato.started, Integer)).label("started"),
-            func.sum(func.cast(Tomato.completed, Integer)).label("completed"),
-            func.sum(func.cast(Tomato.interrupted, Integer)).label("interrupted"),
-        )
-        .group_by(func.extract("week", Tomato.date))
-        .order_by(func.extract("week", Tomato.date))
-        .all()
-    )
-    print("[green]Weekly Summary[/green]")
-    print(tabulate(rows, headers=["Week", "Started", "Completed", "Interrupted"]))
+	with Session(engine) as session:
+		rows = (
+			session.query(
+				func.extract('week', Tomato.started_at).label('week'),
+				func.count(Tomato.started_at).label('started'),
+				func.count(Tomato.completed_at).label('completed'),
+				func.count(Tomato.cancelled_at).label('cancelled'),
+			)
+			.group_by(func.extract('week', Tomato.started_at))
+			.order_by(func.extract('week', Tomato.started_at))
+			.all()
+		)
+		print('[green]Weekly Summary[/green]')
+		print(tabulate(rows, headers=['Week', 'Started', 'Completed', 'Interrupted']))
 
 
 @app.command()
 def categories():
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    rows = (
-        session.query(
-            Tomato.title,
-            func.sum(func.cast(Tomato.started, Integer)).label("started"),
-            func.sum(func.cast(Tomato.completed, Integer)).label("completed"),
-            func.sum(func.cast(Tomato.interrupted, Integer)).label("interrupted"),
-        )
-        .group_by(Tomato.title)
-        .order_by(func.sum(func.cast(Tomato.started, Integer)).desc())
-        .all()
-    )
-    print("[green]Categories Summary[/green]")
-    print(tabulate(rows, headers=["Category", "Started", "Completed", "Interrupted"]))
+	with Session(engine) as session:
+		rows = (
+			session.query(
+				Tomato.title,
+				func.count(Tomato.started_at).label('started'),
+				func.count(Tomato.completed_at).label('completed'),
+				func.count(Tomato.cancelled_at).label('cancelled'),
+			)
+			.group_by(Tomato.title)
+			.order_by(func.count(Tomato.started_at).desc())
+			.all()
+		)
+		print('[green]Categories Summary[/green]')
+		print(tabulate(rows, headers=['Category', 'Started', 'Completed', 'Interrupted']))
 
 
 sched = typer.Typer()
@@ -165,28 +147,28 @@ sched = typer.Typer()
 
 @sched.command()
 def run():
-    sch = WeeklySchedule()
-    sch.create()
-    try:
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+	sch = WeeklySchedule()
+	sch.create()
+	try:
+		while True:
+			schedule.run_pending()
+			time.sleep(1)
 
-    except KeyboardInterrupt:
-        print(EXIT_MESSAGE)
-        sys.exit(0)
+	except KeyboardInterrupt:
+		print(EXIT_MESSAGE)
+		sys.exit(0)
 
 
-@sched.command(name="weekly")
+@sched.command(name='weekly')
 def print_weekly():
-    sch = WeeklySchedule()
-    sch.print_weekly()
+	sch = WeeklySchedule()
+	sch.print_weekly()
 
 
-@sched.command(name="day")
-def print_day(day: str = typer.Argument("Today")):
-    sch = WeeklySchedule()
-    sch.print_day(day)
+@sched.command(name='day')
+def print_day(day: str = typer.Argument('Today')):
+	sch = WeeklySchedule()
+	sch.print_day(day)
 
 
-app.add_typer(sched, name="schedule")
+app.add_typer(sched, name='schedule')
--- tomato_blocks/tomato_block.py
+++ tomato_blocks/tomato_block.py
@@ -6,88 +6,88 @@ from datetime import datetime
 
 
 class TomatoBlock:
-    def __init__(
-        self,
-        title: str,
-        duration: int = 20,
-        notes: str = "",
-        break_title: str = "",
-        break_minutes: int = 0,
-        break_msg: str = "",
-        max_width: int = 80,
-    ):
-        self.title = f'{datetime.now().strftime("%A %D %I:%M%p")} {title}'
-        self.duration = duration
-        self.notes = notes
-        self.break_title = break_title
-        self.break_minutes = break_minutes
-        self.break_msg = break_msg
-        self.max_width = max_width
+	def __init__(
+		self,
+		title: str,
+		duration: int = 20,
+		notes: str = '',
+		break_title: str = '',
+		break_minutes: int = 0,
+		break_msg: str = '',
+		max_width: int = 80,
+	):
+		self.title = f'{datetime.now().strftime("%A %D %I:%M%p")} {title}'
+		self.duration = duration
+		self.notes = notes
+		self.break_title = break_title
+		self.break_minutes = break_minutes
+		self.break_msg = break_msg
+		self.max_width = max_width
 
-    def _tomato_timer(self, minutes: int) -> None:
-        start_time = time.time()
-        while True:
-            elapsed_seconds = int(round(time.time() - start_time))
-            remaining_seconds = minutes * 60 - elapsed_seconds
-            countdown = f"{int(remaining_seconds / 60):02}:{int(remaining_seconds % 60):02} ⏰"
-            duration = (self.max_width - 16) // 2
-            self._progressbar(elapsed_seconds, minutes * 60, duration, countdown)
+	def _tomato_timer(self, minutes: int) -> None:
+		start_time = time.time()
+		while True:
+			elapsed_seconds = int(round(time.time() - start_time))
+			remaining_seconds = minutes * 60 - elapsed_seconds
+			countdown = f'{int(remaining_seconds / 60):02}:{int(remaining_seconds % 60):02} ⏰'
+			duration = (self.max_width - 16) // 2
+			self._progressbar(elapsed_seconds, minutes * 60, duration, countdown)
 
-            if remaining_seconds <= 0:
-                print()
-                break
-            time.sleep(1)
+			if remaining_seconds <= 0:
+				print()
+				break
+			time.sleep(1)
 
-    def _progressbar(self, curr: int, total: int, duration: int, extra: str = ""):
-        frac = curr / total
-        filled = round(frac * duration)
-        print(
-            f'\r{"🍅" * filled}{"--" * (duration - filled)}[ {frac:.0%} ]{extra}',
-            end="",
-        )
+	def _progressbar(self, curr: int, total: int, duration: int, extra: str = ''):
+		frac = curr / total
+		filled = round(frac * duration)
+		print(
+			f'\r{"🍅" * filled}{"--" * (duration - filled)}[ {frac:.0%} ]{extra}',
+			end='',
+		)
 
-    def _notify(self, title: str, msg: str):
-        """
-        # macos desktop notification
-        terminal-notifier -> https://github.com/julienXX/terminal-notifier#download
-        """
-        with contextlib.suppress(Exception):
-            subprocess.run(
-                [
-                    "terminal-notifier",
-                    "-title",
-                    title,
-                    "-message",
-                    msg,
-                    "-sound",
-                    "default",
-                ]
-            )
+	def _notify(self, title: str, msg: str):
+		"""
+		# macos desktop notification
+		terminal-notifier -> https://github.com/julienXX/terminal-notifier#download
+		"""
+		with contextlib.suppress(Exception):
+			subprocess.run(
+				[
+					'terminal-notifier',
+					'-title',
+					title,
+					'-message',
+					msg,
+					'-sound',
+					'default',
+				]
+			)
 
-    def _print_title(self, is_break=False):
-        space = self.max_width - len(self.title) - 10
-        left_pad = ("⎼" * (space // 2)) + (" " * 5)
-        right_pad = (" " * 5) + ("⎼" * (space // 2))
-        title = f"{left_pad}{self.title}{right_pad}"
-        if not is_break:
-            self._clear_screen()
-            top_border = "⎺" * len(title)
-            bottom_border = "⎽" * len(title)
-            print("\n".join(["", top_border, title, bottom_border, "", ""]))
-        else:
-            print("\n" * 3)
-            print(title)
-            print()
+	def _print_title(self, is_break=False):
+		space = self.max_width - len(self.title) - 10
+		left_pad = ('⎼' * (space // 2)) + (' ' * 5)
+		right_pad = (' ' * 5) + ('⎼' * (space // 2))
+		title = f'{left_pad}{self.title}{right_pad}'
+		if not is_break:
+			self._clear_screen()
+			top_border = '⎺' * len(title)
+			bottom_border = '⎽' * len(title)
+			print('\n'.join(['', top_border, title, bottom_border, '', '']))
+		else:
+			print('\n' * 3)
+			print(title)
+			print()
 
-    def _clear_screen(self):
-        clear = "cls" if os.name == "nt" else "clear"
-        subprocess.call(clear, shell=True)
+	def _clear_screen(self):
+		clear = 'cls' if os.name == 'nt' else 'clear'
+		subprocess.call(clear, shell=True)
 
-    def run(self):
-        self._print_title()
-        self._tomato_timer(self.duration)
-        self._notify(self.title, self.notes)
-        if self.break_minutes > 0:
-            self._print_title(is_break=True)
-            self._tomato_timer(self.break_minutes)
-            self._notify(self.break_title, self.break_msg)
+	def run(self):
+		self._print_title()
+		self._tomato_timer(self.duration)
+		self._notify(self.title, self.notes)
+		if self.break_minutes > 0:
+			self._print_title(is_break=True)
+			self._tomato_timer(self.break_minutes)
+			self._notify(self.break_title, self.break_msg)
